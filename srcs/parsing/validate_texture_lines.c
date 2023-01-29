36d8ea580e8a39d4614d116872022dd20beff828
Sejin Heo <seheo@c4r1s4.42seoul.kr>
2023-01-29 13:05:20 +0900

    feat : xpm 파일 검사

--- srcs/parsing/validate_texture_lines.c
+++ srcs/parsing/validate_texture_lines.c
@@ -6,7 +6,7 @@
 /*   By: seheo <seheo@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/20 23:29:11 by seheo             #+#    #+#             */
-/*   Updated: 2023/01/29 12:59:44 by seheo            ###   ########.fr       */
+/*   Updated: 2023/01/29 13:01:13 by seheo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,11 @@ static void	validate(t_game *game, char *texture_file)
 	fd = read_file(texture_file);
 	if (fd < 0)
 		ft_error(game, "cannot open texture_file");
+	close(fd);
+	if (ft_strlen(texture_file) < ft_strlen(".xpm"))
+		ft_error(game, "texture file name is too short");
+	if (ft_strncmp(texture_file + ft_strlen(texture_file) - 4, ".xpm", 4))
+		ft_error(game, "texture file name must end with \'.xpm\'");
 }
 
 void	validate_texture_lines(t_game *game)
